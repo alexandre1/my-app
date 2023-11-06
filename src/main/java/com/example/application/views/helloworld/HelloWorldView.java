f391b19456688c25ab0e2366ba250aa1d898d0be
alexandre1 <alexjaquet@gmail.com>
2023-11-06 13:21:39 +0100

    update files 4

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -22,7 +22,7 @@ public class HelloWorldView extends HorizontalLayout {
         name = new TextField("Your name");
         sayHello = new Button("Say hello");
         sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+            Notification.show("Hello you are so cute " + name.getValue());
         });
         sayHello.addClickShortcut(Key.ENTER);
 
